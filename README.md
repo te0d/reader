ff44219ed5f78c7c774e8bb3a8b797f9cfe24bf1
Tom O'Donnell <todizzle91@gmail.com>
2014-03-23 19:08:34 -0400

    added tag model, ability to edit feed tags, list feeds by tag; fixed empty summary bug

--- README.md
+++ README.md
@@ -8,5 +8,11 @@ ToDo
 
 * provide security
 * provide reading history
-* provide tags
+* improve tags
+  * remove tag when it has no associations
+  * aggregate feeds by tag
 * integrate forms of social media as feeds, e.g. Twitter
+* validate input
+  * avoid duplicate feeds
+  * only allow alphanumeric tag names
+* provide offline capability
--- reader.rb
+++ reader.rb
@@ -11,24 +11,39 @@ class Reader < Sinatra::Base
   
 	  field :url, type: String
 	  field :title, type: String
+    has_and_belongs_to_many :tags
 	end
+
+  class Tag
+    include Mongoid::Document
+
+    field :name, type: String
+    has_and_belongs_to_many :feeds
+  end
 
 	get "/" do 
 	  @feeds = Feed.all
+    @tag_names = Tag.all.pluck(:name)
 
 	  erb :index
 	end
 
 	post "/new" do 
 	  url = params[:url]
+    feed_tags = params[:tags]
     rss = Feedzirra::Feed.fetch_and_parse(url)
     feed = Feed.new
 
     feed.title = rss.title
     feed.url = url
-
 	  feed.save
-	  redirect '/'
+
+    feed_tags.split.each do |tagname|
+      tag = Tag.find_or_create_by(name: tagname) 
+      feed.tags.push(tag)
+    end
+
+    redirect '/'
 	end
 
 	get "/show/:id" do |id|
@@ -41,10 +56,36 @@ class Reader < Sinatra::Base
 	  erb :show
 	end
 
+  get "/edit/:id" do |id|
+    @feed = Feed.find(id)
+
+    erb :edit
+  end
+
+  post "/update" do
+    id = params[:id]
+    feed = Feed.find(id)
+    feed.tags.clear
+    feed_tags = params[:tags].split
+    feed_tags.each do |tagname|
+      tag = Tag.find_or_create_by(name: tagname) 
+      feed.tags.push(tag)
+    end
+    
+    redirect '/show/' + id
+  end
+
 	get "/delete/:id" do |id|
 	  @feed = Feed.find(id)
 	  @feed.delete
 
 	  redirect '/'
 	end
+
+  get "/tags/:name" do |name|
+    @feeds = Tag.find_by(name: name).feeds
+    @tag_names = Tag.all.pluck(:name)
+
+    erb :index
+  end
 end
