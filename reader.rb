b233844687c4ccf46ac0609c265c2c782001f9d0
Tom O'Donnell <todonnel91@gmail.com>
2018-01-09 11:23:54 -0500

    Added Rudimentary Headlines Feature
    Added a new Headline model with the intention of the user being able to select
    certain tags for which a new/trending article will display on the front page for
    that tag. Like a newspaper. Currently, it just updates the headline to the
    newest article for the last clicked feed with that tag.
    
    For one, it would at least be good to check by date. More importantly, feeds
    need to be checked periodically to update what's new for each category. This
    can also provide a list of overall newest articles.
    
    Eventually, using outside sources to determine article popularity would be ideal.
    Comments from those outside sources could also be linked to. And perhaps users
    could even choose which sources they want to use to determine popularity.

--- reader.rb
+++ reader.rb
@@ -22,6 +22,14 @@ class Reader < Sinatra::Base
     has_and_belongs_to_many :feeds
   end
 
+  class Headline
+    include Mongoid::Document
+
+    field :tag_name, type: String
+    field :article_title, type: String
+    field :article_url, type: String
+  end
+
   helpers do
     def protected!
       return if authorized?
@@ -39,6 +47,7 @@ class Reader < Sinatra::Base
     # _id should be a timestamp so sorting on it sorts by creation time
     @recent_feeds = Feed.desc(:_id).limit(5)
     @popular_feeds = Feed.desc(:hit_count).limit(15)
+    @headlines = Headline.all
     @tags = Tag.asc(:name)
 
     erb :welcome
@@ -80,6 +89,24 @@ class Reader < Sinatra::Base
     rss = Feedjira::Feed.fetch_and_parse(@feed.url)
     rss.sanitize_entries!
     @entries = rss.entries
+
+    # Update Headlines
+    # TODO:  Temporary! This should not be done here since headlines will only update "too late".
+    #   Also should use some references instead of doing this find stuff but mongoid complained
+    #   about embedding tag in headline due to tag's relation to feed.
+    headlines = []
+    @feed.tags.each do |tag|
+      headline = Headline.find_by(tag_name: tag.name)
+      headlines.push(headline) if !headline.nil?
+    end
+
+    headlines.each do |headline|
+      if @entries.length > 0
+        headline.article_title = @entries[0].title
+        headline.article_url = @entries[0].url
+        headline.save
+      end
+    end
     
     erb :show
   end
@@ -117,7 +144,21 @@ class Reader < Sinatra::Base
   get "/feeds/tags/:name" do |name|
     @feeds = Tag.find_by(name: name).feeds
     @tags = Tag.asc(:name)
+    @tag_name = name
+
+    # TODO: Would be better if tag knew if it is a headline.
+    @isHeadline = !Headline.find_by(tag_name: name).nil?
 
     erb :index
+  end
+
+  get "/headlines/new/:tag_name" do |tag_name|
+    protected!
+
+    headline = Headline.new
+    headline.tag_name = tag_name
+    headline.save
+
+    redirect '/feeds/tags/' + tag_name
   end
 end
